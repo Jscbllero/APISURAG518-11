87b9ba321e452f3a912191f2708edf3dd09a8061
Joan Sebastian Caballero Q. <sbcbllero@gmail.com>
2024-11-18 09:17:40 -0500

    Evento evaluativo

---
+++ apisurag5-2(actualizada)/src/main/java/com/example/ServidorSura5/Servicios/ServiciosPaciente.java
@@ -0,0 +1,26 @@
+package com.example.ServidorSura5.Servicios;
+
+import com.example.ServidorSura5.MODELOS.Paciente;
+import com.example.ServidorSura5.Repositorios.IRepositorioPaciente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServiciosPaciente {
+    //llamar al respositorio respectivo
+
+    @Autowired
+    IRepositorioPaciente iRepositorioPaciente;
+
+    //se programa las funciones para las distintas opercaiones en bd
+
+    public Paciente guardarPaciente(Paciente datosPaciente)throws Exception{
+        try{
+            return iRepositorioPaciente.save(datosPaciente);
+
+        }catch(Exception error){
+            throw new Exception(error.getMessage());
+        }
+    }
+
+}
